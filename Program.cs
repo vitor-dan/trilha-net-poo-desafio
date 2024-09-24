f22274e225ead5a22124c8b612677a4b9b2a9d04
Vitor Daniel <vitor-vdbs_credenciada@avodtech.com>
2024-09-23 22:09:27 -0300

    Refactor smartphone classes and implement missing properties and methods

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia celularNokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+Iphone celularIphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+
+celularNokia.Ligar();
+celularNokia.ReceberLigacao();
+celularNokia.InstalarAplicativo("WhatsApp");
+
+celularIphone.Ligar();
+celularIphone.ReceberLigacao();
+celularIphone.InstalarAplicativo("Instagram");
